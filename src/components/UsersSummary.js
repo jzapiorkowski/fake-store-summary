539130e5aef2813706118edf4ebe1fadb22c308b
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-04-26 17:40:20 +0200

    render FurthestUsers component

--- src/components/UsersSummary.js
+++ src/components/UsersSummary.js
@@ -1,31 +1,42 @@
 import React from 'react';
+import findTwoFurthestUsers from '../utils/FindFurthestUsers';
+import FurthestUsers from './FurthestUsers';
 
 function UsersSummary({ users }) {
+  const [firstUser, secondUser, maxDistance] = findTwoFurthestUsers(users);
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Username</th>
-          <th>First name</th>
-          <th>Last name</th>
-          <th>City</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((user, id) => {
-          return (
-            <tr key={id}>
-              <td>{user.id}</td>
-              <td>{user.username}</td>
-              <td>{user.name.firstname}</td>
-              <td>{user.name.lastname}</td>
-              <td>{user.address.city}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <>
+      <FurthestUsers
+        firstUser={firstUser}
+        secondUser={secondUser}
+        distance={maxDistance}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>City</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, id) => {
+            return (
+              <tr key={id}>
+                <td>{user.id}</td>
+                <td>{user.username}</td>
+                <td>{user.name.firstname}</td>
+                <td>{user.name.lastname}</td>
+                <td>{user.address.city}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 }
 
