4e27c100067d4c13bac2d8b907e424fe373b6f34
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-04-26 17:39:46 +0200

    create FurtherstUsers component

---
+++ src/components/FurthestUsers.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import SocialDistanceIcon from '@mui/icons-material/SocialDistance';
+
+function FurthestUsers({ firstUser, secondUser, distance }) {
+  return (
+    <div>
+      <table className='user'>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>City</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{firstUser.id}</td>
+            <td>{firstUser.username}</td>
+            <td>{firstUser.name.firstname}</td>
+            <td>{firstUser.name.lastname}</td>
+            <td>{firstUser.address.city}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div className='distance'>
+        {distance.toFixed(2)}
+        <SocialDistanceIcon />
+      </div>
+      <table className='user'>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>City</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{secondUser.id}</td>
+            <td>{secondUser.username}</td>
+            <td>{secondUser.name.firstname}</td>
+            <td>{secondUser.name.lastname}</td>
+            <td>{secondUser.address.city}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default FurthestUsers;
