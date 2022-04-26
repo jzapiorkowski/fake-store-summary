68a720502e84bf5793e369a7b5c2abb8ac7befdb
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-04-26 13:59:00 +0200

    create temporary promise to handle undefined

--- src/App.js
+++ src/App.js
@@ -6,18 +6,27 @@ function App() {
   const [users, setUsers] = useState([]);
   const [carts, setCarts] = useState([]);
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    FakeStoreClient.get('/users').then((response) => {
+    const p1 = FakeStoreClient.get('/users').then((response) => {
       setUsers(response.data);
     });
-    FakeStoreClient.get('/carts').then((response) => {
+    const p2 = FakeStoreClient.get('/carts').then((response) => {
       setCarts(response.data);
     });
-    FakeStoreClient.get('/products').then((response) => {
+    const p3 = FakeStoreClient.get('/products').then((response) => {
       setProducts(response.data);
     });
+
+    Promise.all([p1, p2, p3]).then(() => {
+      setIsLoading(false);
+    });
   }, []);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <div className='App'>
