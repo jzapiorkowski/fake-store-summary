ff068d83d02f5fc70ebce67a70402ae4e7677235
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-04-28 17:15:42 +0200

    fix nesting error

--- src/components/summaryTabs/SummaryTabs.js
+++ src/components/summaryTabs/SummaryTabs.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import CartsSummary from '../cartsSummary/CartsSummary';
 import CategorySummary from '../categorySummary/CategorySummary';
@@ -60,11 +59,7 @@ function TabPanel(props) {
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
     </div>
   );
 }
